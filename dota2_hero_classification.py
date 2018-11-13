9f9f45debe4647789f7b7b3f68c4951df2305473
Muhammad Faqih Dzulqarnain <31657395+mfaqihdzulqarnain@users.noreply.github.com>
2018-11-13 18:13:18 +0700

    Program update

--- dota2_hero_classification.py
+++ dota2_hero_classification.py
@@ -4,71 +4,62 @@ from keras.layers import Input, Dense
 from keras.optimizers import SGD
 from keras.utils.np_utils import to_categorical
 
-# Pandas read CSV
+# Membaca file CSV untuk data training
 sf_train = pd.read_csv('training_data.csv')
 
-# Correlation Matrix for target
+# Menjadikan matrix korelasi untuk target csv
 corr_matrix = sf_train.corr()
 print(corr_matrix['type'])
 
-# Drop unnecessary columns
+# Membuang kolom yang tidak diperlukan
 sf_train.drop(sf_train.columns[[5, 12, 14, 21, 22, 23]], axis=1, inplace=True)
 print(sf_train.head())
 
 #%%
-# Pandas read CSV
-sf_train = pd.read_csv('training_data.csv')
 
-# Correlation Matrix for target
-corr_matrix = sf_train.corr()
-print(corr_matrix['type'])
-
-# Drop unnecessary columns
-sf_train.drop(sf_train.columns[[5, 12, 14, 21, 22, 23]], axis=1, inplace=True)
-print(sf_train.head())
-
-# Pandas read Validation CSV
+# Membaca file csv untuk data testing
 sf_val = pd.read_csv('testing_data.csv')
 
-# Drop unnecessary columns
+# Membuang kolom yang tidak dibutuhkan
 sf_val.drop(sf_val.columns[[5, 12, 14, 21, 22, 23]], axis=1, inplace=True)
 
-# Get Pandas array value (Convert to NumPy array)
+# Merubah data dari library panda menjadi data array
 train_data = sf_train.values
 val_data = sf_val.values
 
-# Use columns 2 to last as Input
+# Menggunakan kolom kedua sebagai data training dan testing
 train_x = train_data[:,2:]
 val_x = val_data[:,2:]
 
-# Use columns 1 as Output/Target (One-Hot Encoding)
+# Menggunakan kolom pertama untuk target output
 train_y = to_categorical( train_data[:,1] )
 val_y = to_categorical( val_data[:,1] )
 
 #%%
-# Create Network
+# Membuat network
 inputs = Input(shape=(16,))
 h_layer = Dense(10, activation='sigmoid')(inputs)
 
-# Softmax Activation for Multiclass Classification
+# Softmax Activation untuk klasifikasi multi kelas
 outputs = Dense(3, activation='softmax')(h_layer)
 
 model = Model(inputs=inputs, outputs=outputs)
 
-# Optimizer / Update Rule
+# Optimizer
 sgd = SGD(lr=0.001)
+#adam = Adam(lr=0.001)
 
-# Compile the model with Cross Entropy Loss
+# Compile model
 model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model and use validation data
+# Melatih model menggunakan data testing
 model.fit(train_x, train_y, batch_size=16, epochs=5000, verbose=1, validation_data=(val_x, val_y))
-model.save_weights('weights.h5')
+#model.save_weights('weights.h5')
 
-# Predict all Validation data
+# Memprediksi semua data testing
 predict = model.predict(val_x)
 
-# Visualize Prediction
+# Menampilkan prediksi
 df = pd.DataFrame(predict)
 df.columns = [ 'Strength', 'Agility', 'Intelligent' ]
 df.index = val_data[:,0]
